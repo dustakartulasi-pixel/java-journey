fdeeba3ad5e18862a81c9906eb98c1c1f90c7a0d
Tulasi Bai <dustakartulasi@gmail.com>
2026-08-17 12:59:56 +0530

    Array 1st day

--- Week03/BeginnerArray.java
+++ Week03/BeginnerArray.java
@@ -7,10 +7,12 @@ public class BeginnerArray {
         Scanner sc = new Scanner(System.in);
         int[] marks = new int[5];
         for (int i = 0; i < marks.length; i++) {
-            System.out.println("Enter Array value of index : " + i);
+            System.out.println("Enter Array value of index" + i + ": ");
             marks[i] = sc.nextInt();
         }
-        for (int i = 0; i <= marks.length - 1; i++) {
+
+        // for (int i = 0; i <= marks.length - 1; i++) {
+        for (int i = marks.length - 1; i >= 0; i--) {
             System.out.println(marks[i]);
         }
         sc.close();
