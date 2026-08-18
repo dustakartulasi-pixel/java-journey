95889eff8f066871a6e13bc28d929729386bb163
Tulasi Bai <dustakartulasi@gmail.com>
2026-08-18 11:27:49 +0530

    add sir MaxElement in Array

---
+++ Week03/MaxEle.java
@@ -0,0 +1,14 @@
+package Week03;
+
+public class MaxEle {
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 5, 30, 15 };
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        System.out.println("The maximum element in the array is: " + max);
+    }
+}
