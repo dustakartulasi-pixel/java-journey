174d0767e5303ba5dc3ab06ade747408388a6f94
Tulasi Bai <dustakartulasi@gmail.com>
2026-08-18 11:30:15 +0530

    add sir MinElement in Array

--- Week03/MaxEle.java
+++ Week03/MaxEle.java
@@ -3,12 +3,21 @@ package Week03;
 public class MaxEle {
     public static void main(String[] args) {
         int[] arr = { 10, 20, 5, 30, 15 };
-        int max = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] > max) {
-                max = arr[i];
+        /*
+         * int max = arr[0];
+         * for (int i = 1; i < arr.length; i++) {
+         * if (arr[i] > max) {
+         * max = arr[i];
+         * }
+         * }
+         * System.out.println("The maximum element in the array is: " + max);
+         */
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
             }
         }
-        System.out.println("The maximum element in the array is: " + max);
+        System.out.println("The minimum element in the array is: " + min);
     }
 }
