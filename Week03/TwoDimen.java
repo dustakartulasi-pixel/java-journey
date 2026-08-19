2269db6c93f615ae31d26e3472d9308ca9a48d1a
Tulasi Bai <dustakartulasi@gmail.com>
2026-08-19 11:57:24 +0530

    2D array

---
+++ Week03/TwoDimen.java
@@ -0,0 +1,22 @@
+package Week03;
+
+public class TwoDimen {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3];
+        int count = 1;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = count;
+                count++;
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
