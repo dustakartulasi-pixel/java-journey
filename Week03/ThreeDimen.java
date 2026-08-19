e2adeaee4d2666aef2ba98b217d1e906b2825091
Tulasi Bai <dustakartulasi@gmail.com>
2026-08-19 19:20:00 +0530

    KodNest Pratice

--- Week03/ThreeDimen.java
+++ Week03/ThreeDimen.java
@@ -1,7 +1,7 @@
 package Week03;
 
 import java.util.Scanner;
-
+/*
 public class ThreeDimen {
     public static void main(String[] args) {
 
@@ -24,6 +24,43 @@ public class ThreeDimen {
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 2; j++) {
                 for (int k = 0; k < 2; k++) {
+                    System.out.print(arr[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
+}*/
+import java.util.Scanner;
+
+public class ThreeDimen {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the dimension blocks of array");
+        int x = sc.nextInt();
+        System.out.println("Enter the dimension rows of array");
+        int y = sc.nextInt();
+        System.out.println("Enter the dimension columns of array");
+        int z = sc.nextInt();
+
+        int[][][] arr = new int[x][y][z];
+
+        // Take values
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                for (int k = 0; k < z; k++) {
+                    System.out.println("Enter value for index [" + i + "][" + j + "][" + k + "]:");
+                    arr[i][j][k] = sc.nextInt();
+                }
+            }
+        }
+        System.out.println("The 3D array elements are:");
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                for (int k = 0; k < z; k++) {
                     System.out.print(arr[i][j][k] + " ");
                 }
                 System.out.println();
