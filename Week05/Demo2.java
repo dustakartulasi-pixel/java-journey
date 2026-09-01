a943837a7578e6cebe645afb8cd335a0e8598d25
Tulasi Bai <dustakartulasi@gmail.com>
2026-09-01 15:04:52 +0530

    KodNest Pratice to copy array and remove element in sepecified index

--- Week05/Demo2.java
+++ Week05/Demo2.java
@@ -2,16 +2,33 @@ package Week05;
 
 public class Demo2 {
     public static void main(String[] args) {
-        int[] a = { 1, 2, 4, 5 };
-        int[] b = new int[5];
-        int element = 3;
+        /*
+         * int[] a = { 1, 2, 4, 5 };
+         * int[] b = new int[5];
+         * int element = 3;
+         * int index = 2;
+         * for (int i = 0; i < index; i++) {
+         * b[i] = a[i];
+         * }
+         * b[index] = element;
+         * for (int i = index; i < a.length; i++) {
+         * b[i + 1] = a[i];
+         * }
+         * for (int i = 0; i < b.length; i++) {
+         * System.out.println(b[i]);
+         * }
+         */
+        int[] a = { 1, 2, 3, 4, 5 };
+        int[] b = new int[4];
         int index = 2;
-        for (int i = 0; i < index; i++) {
-            b[i] = a[i];
-        }
-        b[index] = element;
-        for (int i = index; i < a.length; i++) {
-            b[i + 1] = a[i];
+        int element = 3;
+        for (int i = 0; i < b.length; i++) {
+            if (i < index) {
+                b[i] = a[i];
+
+            } else {
+                b[i] = a[i + 1];
+            }
         }
         for (int i = 0; i < b.length; i++) {
             System.out.println(b[i]);
