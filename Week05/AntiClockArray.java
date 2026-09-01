c84bbede57e8fda4d977841dc0772ef893d2f14b
Tulasi Bai <dustakartulasi@gmail.com>
2026-09-01 15:14:04 +0530

    KodNest Pratice to copy array and remove from antiClockWise

---
+++ Week05/AntiClockArray.java
@@ -0,0 +1,18 @@
+package Week05;
+
+public class AntiClockArray {
+    public static void main(String[] args) {
+        int[] a = { 10, 20, 30, 40, 50 };
+        int[] b = new int[a.length];
+        int first = a[0];
+        for (int i = 0; i < a.length - 1; i++) {
+            b[i] = a[i + 1];
+        }
+        b[a.length - 1] = first;
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(b[i]);
+        }
+    }
+
+}
