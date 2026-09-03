7d9e413e48320de76d700100896f2e3e21dde64f
Tulasi Bai <dustakartulasi@gmail.com>
2026-09-03 15:26:05 +0530

    KodNest Pratice using PREFIX :)

---
+++ Week05/Prefix.java
@@ -0,0 +1,22 @@
+package Week05;
+
+import java.util.Arrays;
+
+public class Prefix {
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 30, 40, 50 };
+        int[] prefixSum = new int[arr.length];
+        System.out.println("Original Array is: " + Arrays.toString(arr));
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            prefixSum[i] = sum;
+        }
+        System.out.println("Prefix Sum Array is: " + Arrays.toString(prefixSum));
+        // for (int i = 0; i < prefixSum.length; i++) {
+        // System.out.print(prefixSum[i] + " ");
+        // }
+
+    }
+
+}
