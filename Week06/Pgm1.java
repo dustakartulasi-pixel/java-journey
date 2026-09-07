73b2a925fe56a92fe249e7f1c5a3c51ffa8027db
Tulasi Bai <dustakartulasi@gmail.com>
2026-09-07 17:30:02 +0530

    sample program

---
+++ Week06/Pgm1.java
@@ -0,0 +1,16 @@
+package Week06;
+
+import java.util.Scanner;
+
+class pgm1 {
+    public static void main(String[] args) {
+        String s1 = "Java1";
+        String s2 = "Java";
+        if (s1 == s2) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
+
+    }
+}
