c95d81a6a6090eeab5973dc7a83cf267ca850298
Tulasi Bai <dustakartulasi@gmail.com>
2026-09-07 17:31:03 +0530

    KodNest Pratice using String:)

--- Week06/Pgm1.java
+++ Week06/Pgm1.java
@@ -1,12 +1,10 @@
 package Week06;
 
-import java.util.Scanner;
-
 class pgm1 {
     public static void main(String[] args) {
         String s1 = "Java1";
         String s2 = "Java";
-        if (s1 == s2) {
+        if (s1.equals(s2)) {
             System.out.println("Equal");
         } else {
             System.out.println("Not Equal");
