aff62f8c12b2c8d1f3740eaa75f049ddb89ab7c6
Tulasi Bai <dustakartulasi@gmail.com>
2026-09-08 16:48:52 +0530

    add Pgm2

--- Week06/Pgm2.java
+++ Week06/Pgm2.java
@@ -4,6 +4,11 @@ public class Pgm2 {
     public static void main(String[] args) {
         String s1 = new String("Java");
         String s2 = new String("JaVa");
+        if (s1 == s2) {
+            System.out.println("Both refer to same object");
+        } else {
+            System.out.println("Both refer to different object");
+        }
         if (s1.equalsIgnoreCase(s2)) {
             System.out.println("Equal");
         } else {
