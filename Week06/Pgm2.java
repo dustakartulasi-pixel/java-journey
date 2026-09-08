9975e70f0609e279c216fe80e23822dfc52bdd8d
Tulasi Bai <dustakartulasi@gmail.com>
2026-09-08 16:47:59 +0530

    add Pgm2

--- Week06/Pgm2.java
+++ Week06/Pgm2.java
@@ -3,8 +3,8 @@ package Week06;
 public class Pgm2 {
     public static void main(String[] args) {
         String s1 = new String("Java");
-        String s2 = new String("Java");
-        if (s1.equals(s2)) {
+        String s2 = new String("JaVa");
+        if (s1.equalsIgnoreCase(s2)) {
             System.out.println("Equal");
         } else {
             System.out.println("Not Equal");
