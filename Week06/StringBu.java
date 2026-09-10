4dc0d04688c73f10c500402195a67e9a9a3be80d
Tulasi Bai <dustakartulasi@gmail.com>
2026-09-10 20:34:21 +0530

    String Builder capacity

---
+++ Week06/StringBu.java
@@ -0,0 +1,13 @@
+package Week06;
+
+/**
+ * StringBu
+ */
+public class StringBu {
+
+    public static void main(String args[]) {
+        StringBuilder s1 = new StringBuilder();
+        System.out.println(s1.capacity());
+
+    }
+}
