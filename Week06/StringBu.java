989b1babe3d6e9e95c93c33d2bb47670de61e0df
Tulasi Bai <dustakartulasi@gmail.com>
2026-09-10 20:40:00 +0530

    String Builder capacity

--- Week06/StringBu.java
+++ Week06/StringBu.java
@@ -8,6 +8,16 @@ public class StringBu {
     public static void main(String args[]) {
         StringBuilder s1 = new StringBuilder();
         System.out.println(s1.capacity());
+        System.out.println(s1.length());
+        s1.append("Java");
+        System.out.println(s1);
+        System.out.println(s1.length());
+        System.out.println(s1.capacity());
+        System.out.println(s1.length());
+        s1.append(" is a programming language");
+        System.out.print(s1);
+        System.out.println(s1.capacity());
+        System.out.println(s1.length());
 
     }
 }
